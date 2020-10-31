b6b93c81ff2c217afa9aa5c6e4f5165dc7206cc1
Sai Kumar <psvnlsaikumar@gmail.com>
2020-11-01 03:24:17 +0530

    Added api to get weather data based on latitude and longitude

--- server/src/api/weather.js
+++ server/src/api/weather.js
@@ -1,11 +1,27 @@
 const { Router } = require("express");
 const { default: fetch } = require("node-fetch");
 require("dotenv").config({ path: __dirname + "/.env" });
+
 const router = Router();
 
-router.get("/", (req, res) => {
-  let city = "cebu";
+router.get("/by_city/", (req, res) => {
+  let city = req.body.city;
+  console.log(`${city}city`);
   const url = `${process.env.WEATHER_API}weather?q=${city}&appid=${process.env.WEATHER_API_KEY}`;
+  console.log(url);
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((response) => res.send(response))
+    .catch((err) => {
+      return new Error(err);
+    });
+});
+
+router.get("/by_lat_lon/", (req, res) => {
+  let lat = req.body.lat;
+  let lon = req.body.lon;
+  const url = `${process.env.WEATHER_API}weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`;
   console.log(url);
 
   fetch(url)
